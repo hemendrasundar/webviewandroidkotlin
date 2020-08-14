53d91f716be135d00a7bb50c7a2f86c839477d0d
hemu <hemendraoid>
2020-08-14 08:05:07 +0530

    initial commit

---
+++ app/src/main/java/com/hemendra/webview/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.hemendra.webview
+
+import android.content.Context
+import android.graphics.Bitmap
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        webview.settings.javaScriptEnabled =true
+        webview.settings.builtInZoomControls=true
+        webview.webViewClient = myWebviewClient(this@MainActivity)
+        btn.setOnClickListener {
+
+           webview.loadUrl("https://${et.text.toString()}")
+       }
+
+    }
+
+    class myWebviewClient(var context:Context):WebViewClient()
+    {
+
+        override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+            super.onPageStarted(view, url, favicon)
+            Toast.makeText(context,"page loading started...",Toast.LENGTH_SHORT).show()
+        }
+
+        override fun onPageFinished(view: WebView?, url: String?) {
+            super.onPageFinished(view, url)
+            Toast.makeText(context,"page loaded successfully...!!!",Toast.LENGTH_SHORT).show()
+        }
+        override fun shouldOverrideUrlLoading(view: WebView?,url:String): Boolean {
+
+            view?.loadUrl(url)
+
+            return true
+        }
+    }
+}
